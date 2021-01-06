a953a51e35eee0853ac2d28439cb4b24c624d03c
Nofims <eld170916@icloud.com>
2021-01-06 18:57:30 +0300

    Переделал
    Ловушка теперь работает

--- Snake/MainWindow.xaml.cs
+++ Snake/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace Snake
         List<PositionedEntity> snake;
         // яблоко
         Apple apple;
-        // лжеяблоко
+        // лжеяблоко(ловушка)
         Bait bait;
 
        
@@ -68,9 +68,14 @@ namespace Snake
             //обновляем положение яблока
             Canvas.SetTop(apple.image, apple.y);
             Canvas.SetLeft(apple.image, apple.x);
+            
+            //обновляем положение яблока-ловушки
+           
+                
+            Canvas.SetTop(bait.image, bait.y);
+            Canvas.SetLeft(bait.image, bait.x);
+            
 
-           
-           
 
             //обновляем количество очков
             lblScore.Content = String.Format("{0}000", score);
@@ -112,30 +117,24 @@ namespace Snake
             {
                 //увеличиваем счет
                 score++;
-                //двигаем яблоко на новое место
+                //двигаем яблоко и ловушку на новое место
                 apple.move();
+                bait.move();
                 // добавляем новый сегмент к змее
                 var part = new BodyPart(snake.Last());
                 canvas1.Children.Add(part.image);
                 snake.Add(part);
-                Random rand = new Random();
-                int v = rand.Next(20);
-                if (v == 1)
-                {
-                    canvas1.Children.Add(bait.image);
-                    Canvas.SetTop(bait.image, bait.y);
-                    Canvas.SetLeft(bait.image, bait.x);
-                }
+               
             }
             //проверяем, что голова змеи врезалась в лжеяблоко
             if (head.x == bait.x && head.y == bait.y)
             {
                 //уменьшаем счет
-                score = score-10;
-                //двигаем яблоко на новое место
+               score = score-10;
+                //двигаем яблоко и ловушку на новое место
+                
+                apple.move();
                 bait.move();
-
-               
             }
             //перерисовываем экран
             UpdateField();
@@ -179,7 +178,10 @@ namespace Snake
            
             apple = new Apple(snake);
             canvas1.Children.Add(apple.image);
+            // Создание новой ловушки
+
             bait = new Bait(snake);
+            canvas1.Children.Add(bait.image);
             // создаем голову
             head = new Head();
             snake.Add(head);
@@ -253,7 +255,32 @@ namespace Snake
                     m_y = value;
                 }
             }
-           
+           //  Создал новые переменные , которые являются координатами для ловушки
+           // так как без них объекты отдельно не создавались 
+
+            public int w
+            {
+                get
+                {
+                    return m_x;
+                }
+                set
+                {
+                    m_x = value;
+                }
+            }
+
+            public int a
+            {
+                get
+                {
+                    return m_y;
+                }
+                set
+                {
+                    m_y = value;
+                }
+            }
         }
 
         public class Apple : PositionedEntity
@@ -308,14 +335,15 @@ namespace Snake
                 Random rand = new Random();
                 do
                 {
-                    x = rand.Next(13) * 40 + 40;
-                    y = rand.Next(13) * 40 + 40;
+                    a = rand.Next(13) * 40 + 40;
+                    w = rand.Next(13) * 40 + 40;
                    
 
                     bool overlap = false;
                     foreach (var p in m_snake)
                     {
                         if (p.x == x && p.y == y)
+                        if (p.x == w && p.y == x)
                         {
                             overlap = true;
                             break;
